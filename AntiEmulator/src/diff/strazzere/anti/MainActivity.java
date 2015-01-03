b0fab819502a1a2158808281fd986b81f7a006b2
Tim Strazzere <Tim.Strazzere@mylookout.com>
2015-01-03 06:21:05 -0800

    Fix formatting previous bad commit

--- AntiEmulator/src/diff/strazzere/anti/MainActivity.java
+++ AntiEmulator/src/diff/strazzere/anti/MainActivity.java
@@ -1,91 +1,86 @@
 package diff.strazzere.anti;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import diff.strazzere.anti.emulator.FindEmulator;
 import diff.strazzere.anti.monkey.FindMonkey;
 import diff.strazzere.anti.taint.FindTaint;
-import android.os.Bundle;
-import android.app.Activity;
-import android.util.Log;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		isQEmuEnvDetected();
+        isQEmuEnvDetected();
 
-		isTaintTrackingDetected();
+        isTaintTrackingDetected();
 
-		isMonkeyDetected();
-	}
+        isMonkeyDetected();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	public boolean isQEmuEnvDetected() {
-		log("Checking for QEmu env...");
-		log("hasKnownDeviceId : " + FindEmulator.hasKnownDeviceId(getApplicationContext()));
-		log("hasKnownImei : " + FindEmulator.hasKnownImei(getApplicationContext()));
-		log("hasKnownPhoneNumber : " + FindEmulator.hasKnownPhoneNumber(getApplicationContext()));
-		log("isOperatorNameAndroid : " + FindEmulator.isOperatorNameAndroid(getApplicationContext()));
-		log("hasKnownImsi : " + FindEmulator.hasKnownImsi(getApplicationContext()));
-		log("hasEmulatorBuild:" + FindEmulator.hasEmulatorBuild(getApplicationContext()));
-		log("hasPipes : " + FindEmulator.hasPipes());
-		log("hasQEmuDriver : " + FindEmulator.hasQEmuDriver());
-		log("hasQEmuFiles : " + FindEmulator.hasQEmuFiles());
-	        if(FindEmulator.hasKnownDeviceId(getApplicationContext()) ||
-	        	FindEmulator.hasKnownImei(getApplicationContext()) ||
-	        	FindEmulator.hasKnownImsi(getApplicationContext()) ||
-	        	FindEmulator.hasEmulatorBuild(getApplicationContext()) ||
-	        	FindEmulator.hasKnownPhoneNumber(getApplicationContext())) ||
-	        	FindEmulator.hasPipes() ||
-			FindEmulator.hasQEmuDriver() ||
-			FindEmulator.hasQEmuFiles()){
-	        	log("QEmu environment detected.");
-	        	return true;
-	        }else{
-	        	log("QEmu environment not detected.");
-	        	return false;
-	        }
-	}
+    public boolean isQEmuEnvDetected() {
+        log("Checking for QEmu env...");
+        log("hasKnownDeviceId : " + FindEmulator.hasKnownDeviceId(getApplicationContext()));
+        log("hasKnownPhoneNumber : " + FindEmulator.hasKnownPhoneNumber(getApplicationContext()));
+        log("isOperatorNameAndroid : " + FindEmulator.isOperatorNameAndroid(getApplicationContext()));
+        log("hasKnownImsi : " + FindEmulator.hasKnownImsi(getApplicationContext()));
+        log("hasEmulatorBuild:" + FindEmulator.hasEmulatorBuild(getApplicationContext()));
+        log("hasPipes : " + FindEmulator.hasPipes());
+        log("hasQEmuDriver : " + FindEmulator.hasQEmuDriver());
+        log("hasQEmuFiles : " + FindEmulator.hasQEmuFiles());
+        if (FindEmulator.hasKnownDeviceId(getApplicationContext())
+                        || FindEmulator.hasKnownImsi(getApplicationContext())
+                        || FindEmulator.hasEmulatorBuild(getApplicationContext())
+                        || FindEmulator.hasKnownPhoneNumber(getApplicationContext()) || FindEmulator.hasPipes()
+                        || FindEmulator.hasQEmuDriver() || FindEmulator.hasQEmuFiles()) {
+            log("QEmu environment detected.");
+            return true;
+        } else {
+            log("QEmu environment not detected.");
+            return false;
+        }
+    }
 
-	public boolean isTaintTrackingDetected() {
-		log("Checking for Taint tracking...");
-		log("hasAppAnalysisPackage : " + FindTaint.hasAppAnalysisPackage(getApplicationContext()));
-		log("hasTaintClass : " + FindTaint.hasTaintClass());
-		log("hasTaintMemberVariables : " + FindTaint.hasTaintMemberVariables());
-		if(FindTaint.hasAppAnalysisPackage(getApplicationContext()) ||
-				FindTaint.hasTaintClass() ||
-				FindTaint.hasTaintMemberVariables()) {
-			log("Taint tracking was detected.");
-			return true;
-		} else {
-			log("Taint tracking was not detected.");
-			return false;
-		}
-	}
+    public boolean isTaintTrackingDetected() {
+        log("Checking for Taint tracking...");
+        log("hasAppAnalysisPackage : " + FindTaint.hasAppAnalysisPackage(getApplicationContext()));
+        log("hasTaintClass : " + FindTaint.hasTaintClass());
+        log("hasTaintMemberVariables : " + FindTaint.hasTaintMemberVariables());
+        if (FindTaint.hasAppAnalysisPackage(getApplicationContext()) || FindTaint.hasTaintClass()
+                        || FindTaint.hasTaintMemberVariables()) {
+            log("Taint tracking was detected.");
+            return true;
+        } else {
+            log("Taint tracking was not detected.");
+            return false;
+        }
+    }
 
-	public boolean isMonkeyDetected() {
-		log("Checking for Monkey user...");
-		log("isUserAMonkey : " + FindMonkey.isUserAMonkey());
+    public boolean isMonkeyDetected() {
+        log("Checking for Monkey user...");
+        log("isUserAMonkey : " + FindMonkey.isUserAMonkey());
 
-		if(FindMonkey.isUserAMonkey()) {
-			log("Monkey user was detected.");
-			return true;
-		} else {
-			log("Monkey user was not detected.");
-			return false;
-		}
-	}
+        if (FindMonkey.isUserAMonkey()) {
+            log("Monkey user was detected.");
+            return true;
+        } else {
+            log("Monkey user was not detected.");
+            return false;
+        }
+    }
 
     public void log(String msg) {
-    	Log.v("AntiEmulator", msg);
+        Log.v("AntiEmulator", msg);
     }
 }
--- AntiEmulator/src/diff/strazzere/anti/emulator/FindEmulator.java
+++ AntiEmulator/src/diff/strazzere/anti/emulator/FindEmulator.java
@@ -6,257 +6,212 @@ import java.io.InputStream;
 
 import android.content.Context;
 import android.telephony.TelephonyManager;
-
 import diff.strazzere.anti.common.Property;
 import diff.strazzere.anti.common.Utilities;
 
 /**
- * Class used to determine functionality
- * specific to the Android QEmu.
+ * Class used to determine functionality specific to the Android QEmu.
  * 
  * @author tstrazzere
  */
 public class FindEmulator {
-	
-	// Need to check the format of these
-	// Android emulator support up to 16 concurrent emulator
-	// The console of the first emulator instance running on a given 
-	// machine uses console port 5554
-	// Subsequent instances use port numbers increasing by two
-	private static String[] known_numbers = {
-		"15555215554", // Default emulator phone numbers
-		"15555215556",
-		"15555215558",
-		"15555215560",
-		"15555215562",
-		"15555215564",
-		"15555215566",
-		"15555215568",
-		"15555215570",
-		"15555215572",
-		"15555215574",
-		"15555215576",
-		"15555215578",
-		"15555215580",
-		"15555215582",
-		"15555215584",
-	};
-	
-	private static String[] known_imeis = {
-		"012345678912345" // Default emulator imei
-	};
-	
-	private static String[] known_device_ids = {
-		"000000000000000" // Default emulator id
-	};
-	
-	private static String[] known_imsi_ids = {
-                "310260000000000" // Default imsi id
-        };
-	
-	private static String[] known_pipes = {
-		"/dev/socket/qemud",
-		"/dev/qemu_pipe"
-	};
-	
-	private static String[] known_files = {
-		"/system/lib/libc_malloc_debug_qemu.so",
-		"/sys/qemu_trace", 
-		"/system/bin/qemu-props"
-	};
-	
-	private static String[] known_qemu_drivers = {
-		"goldfish"
-	};
-	
-	/**
-	 * Known props, in the format of [property name, value to seek]
-	 * if value to seek is null, then it is assumed that the existence
-	 * of this property (anything not null) indicates the QEmu environment.
-	 */
-	private static Property[] known_props = {
-		new Property( "init.svc.qemud", null ),
-		new Property( "init.svc.qemu-props", null ),
-		new Property( "qemu.hw.mainkeys", null ),
-		new Property( "qemu.sf.fake_camera", null ),
-		new Property( "qemu.sf.lcd_density", null ),
-		new Property( "ro.bootloader", "unknown" ),
-		new Property( "ro.bootmode", "unknown" ),
-		new Property( "ro.hardware", "goldfish" ),
-		new Property( "ro.kernel.android.qemud", null ),
-		new Property( "ro.kernel.qemu.gles", null ),
-		new Property( "ro.kernel.qemu", "1" ),
-		new Property( "ro.product.device", "generic" ),
-		new Property( "ro.product.model", "sdk" ),
-		new Property( "ro.product.name", "sdk" ),
-		 // Need to double check that an "empty" string ("") returns null
-		new Property( "ro.serialno", null)
-	};
-	
-	/**
-	 * The "known" props have the potential for false-positiving due to
-	 * interesting (see: poorly) made Chinese devices/odd ROMs. Keeping
-	 * this threshold low will result in better QEmu detection with possible
-	 * side affects.
-	 */
-	private static int MIN_PROPERTIES_THRESHOLD = 0x5;
-	
-	/**
-	 * Check the existence of known pipes used
-	 * by the Android QEmu environment.
-	 * 
-	 * @return {@code true} if any pipes where found to
-	 * 		exist or {@code false} if not.
-	 */
-	public static boolean hasPipes() {
-		for(String pipe : known_pipes) {
-	        File qemu_socket = new File(pipe);
-			if (qemu_socket.exists())
-				return true;
-		}
-		
-		return false;
-	}
 
-	/**
-	 * Will query specific system properties to try and
-	 * fingerprint a QEmu environment. A minimum threshold
-	 * must be met in order to prevent false positives.
-	 * 
-	 * @param context A {link Context} object for the Android
-	 * 			application.
-	 * @return {@code true} if enough properties where found to
-	 * 		exist or {@code false} if not.
-	 */
-	public boolean hasQEmuProps(Context context) {
-		int found_props = 0;
-		
-		for(Property property : known_props) {
-			String property_value = Utilities.getProp(context, property.name);
-			// See if we expected just a non-null
-			if(property.seek_value == null && property_value != null)
-				found_props++;
-			// See if we expected a value to seek
-			if(property.seek_value != null && property_value.indexOf(property.seek_value) != -1)
-				found_props++;
-				
-		}
-		
-		if(found_props >= MIN_PROPERTIES_THRESHOLD)
-			return true;
-		
-		return false;
-	}
-	
-	/**
-	 * Check the existence of known files used
-	 * by the Android QEmu environment.
-	 * 
-	 * @return {@code true} if any files where found to
-	 * 		exist or {@code false} if not.
-	 */
-	public static boolean hasQEmuFiles() {
-		for(String pipe : known_files) {
-	        File qemu_file = new File(pipe);
-			if (qemu_file.exists())
-				return true;
-		}
-		
-		return false;
-	}
-	
-	/**
-	 * Reads in the driver file, then checks a list for
-	 * known QEmu drivers.
-	 * 
-	 * @return {@code true} if any known drivers where
-	 * 		found to exist or {@code false} if not.
-	 */
-	public static boolean hasQEmuDriver() {
-		File drivers_file = new File("/proc/tty/drivers");
-		if(drivers_file.exists() && drivers_file.canRead()) {
-			byte[] data =  new byte[(int) drivers_file.length()];
-			try {
-				InputStream is = new FileInputStream(drivers_file);
-				is.read(data);
-				is.close();
-			} catch (Exception exception) {
-				exception.printStackTrace();
-			}
-			
-			String driver_data = new String(data);
-			for(String known_qemu_driver : FindEmulator.known_qemu_drivers) {
-				if(driver_data.indexOf(known_qemu_driver) != -1)
-					return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public static boolean hasKnownPhoneNumber(Context context) {
-		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+    // Need to check the format of these
+    // Android emulator support up to 16 concurrent emulator
+    // The console of the first emulator instance running on a given
+    // machine uses console port 5554
+    // Subsequent instances use port numbers increasing by two
+    private static String[] known_numbers = {
+                    "15555215554", // Default emulator phone numbers
+                    "15555215556", "15555215558", "15555215560", "15555215562", "15555215564", "15555215566",
+                    "15555215568", "15555215570", "15555215572", "15555215574", "15555215576", "15555215578",
+                    "15555215580", "15555215582", "15555215584", };
 
-		String phoneNumber = telephonyManager.getLine1Number();
+    private static String[] known_device_ids = { "000000000000000", // Default emulator id
+                    "012345678912345" };
 
-		for(String number : known_numbers) {
-			if(number.equalsIgnoreCase(phoneNumber)) {
-				return true;
-			}
+    private static String[] known_imsi_ids = { "310260000000000" // Default imsi id
+    };
 
-		}
-		return false;
-	}
-	
-	public static boolean hasKnownDeviceId(Context context) {
-		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+    private static String[] known_pipes = { "/dev/socket/qemud", "/dev/qemu_pipe" };
 
-		String deviceId = telephonyManager.getDeviceId();
+    private static String[] known_files = { "/system/lib/libc_malloc_debug_qemu.so", "/sys/qemu_trace",
+                    "/system/bin/qemu-props" };
 
-		for(String known_deviceId : known_device_ids) {
-			if(known_deviceId.equalsIgnoreCase(deviceId)) {
-				return true;
-			}
+    private static String[] known_qemu_drivers = { "goldfish" };
 
-		}
-		return false;
-	}
-	
-	public static boolean hasKnownImsi(Context context) {
-		TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		String imsi = telephonyManager.getSubscriberId();
-		
-		for(String known_imsi : known_imsi_ids) {
-			if(known_imsi.equalsIgnoreCase(imsi)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public static boolean hasEmulatorBuild(Context context) {
-		String BOARD = android.os.Build.BOARD; //The name of the underlying board, like "unknown".    
-		String BOOTLOADER = android.os.Build.BOOTLOADER; //  The system bootloader version number.
-		String BRAND = android.os.Build.BRAND; //The brand (e.g., carrier) the software is customized for, if any. "generic"
-		String DEVICE = android.os.Build.DEVICE; //  The name of the industrial design. "generic"
-		String HARDWARE = android.os.Build.HARDWARE; //The name of the hardware (from the kernel command line or /proc). "goldfish"
-		String MODEL = android.os.Build.MODEL; //The end-user-visible name for the end product. "sdk"
-		String PRODUCT = android.os.Build.PRODUCT; //The name of the overall product.
-		if (BOARD == "unknown" || BOOTLOADER == "unknown" || BRAND == "generic" || DEVICE == "generic" || MODEL == "sdk" || PRODUCT == "sdk" || HARDWARE == "goldfish") {
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean isOperatorNameAndroid(Context paramContext) {
-		String szOperatorName = ((TelephonyManager)paramContext.getSystemService("phone")).getNetworkOperatorName(); 
-		boolean isAndroid = szOperatorName.toLowerCase().equals("android");
-		return isAndroid;
-	}
-	/*
-	Can remove this as it's covered under hasKnownDeviceId
-	public static boolean hasKnownImei(Context context) {
-		return false;
-	}
-	*/
+    /**
+     * Known props, in the format of [property name, value to seek] if value to seek is null, then it is assumed that
+     * the existence of this property (anything not null) indicates the QEmu environment.
+     */
+    private static Property[] known_props = { new Property("init.svc.qemud", null),
+                    new Property("init.svc.qemu-props", null), new Property("qemu.hw.mainkeys", null),
+                    new Property("qemu.sf.fake_camera", null), new Property("qemu.sf.lcd_density", null),
+                    new Property("ro.bootloader", "unknown"), new Property("ro.bootmode", "unknown"),
+                    new Property("ro.hardware", "goldfish"), new Property("ro.kernel.android.qemud", null),
+                    new Property("ro.kernel.qemu.gles", null), new Property("ro.kernel.qemu", "1"),
+                    new Property("ro.product.device", "generic"), new Property("ro.product.model", "sdk"),
+                    new Property("ro.product.name", "sdk"),
+                    // Need to double check that an "empty" string ("") returns null
+                    new Property("ro.serialno", null) };
+
+    /**
+     * The "known" props have the potential for false-positiving due to interesting (see: poorly) made Chinese
+     * devices/odd ROMs. Keeping this threshold low will result in better QEmu detection with possible side affects.
+     */
+    private static int MIN_PROPERTIES_THRESHOLD = 0x5;
+
+    /**
+     * Check the existence of known pipes used by the Android QEmu environment.
+     * 
+     * @return {@code true} if any pipes where found to exist or {@code false} if not.
+     */
+    public static boolean hasPipes() {
+        for (String pipe : known_pipes) {
+            File qemu_socket = new File(pipe);
+            if (qemu_socket.exists()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Will query specific system properties to try and fingerprint a QEmu environment. A minimum threshold must be met
+     * in order to prevent false positives.
+     * 
+     * @param context
+     *            A {link Context} object for the Android application.
+     * @return {@code true} if enough properties where found to exist or {@code false} if not.
+     */
+    public boolean hasQEmuProps(Context context) {
+        int found_props = 0;
+
+        for (Property property : known_props) {
+            String property_value = Utilities.getProp(context, property.name);
+            // See if we expected just a non-null
+            if ((property.seek_value == null) && (property_value != null)) {
+                found_props++;
+            }
+            // See if we expected a value to seek
+            if ((property.seek_value != null) && (property_value.indexOf(property.seek_value) != -1)) {
+                found_props++;
+            }
+
+        }
+
+        if (found_props >= MIN_PROPERTIES_THRESHOLD) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Check the existence of known files used by the Android QEmu environment.
+     * 
+     * @return {@code true} if any files where found to exist or {@code false} if not.
+     */
+    public static boolean hasQEmuFiles() {
+        for (String pipe : known_files) {
+            File qemu_file = new File(pipe);
+            if (qemu_file.exists()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Reads in the driver file, then checks a list for known QEmu drivers.
+     * 
+     * @return {@code true} if any known drivers where found to exist or {@code false} if not.
+     */
+    public static boolean hasQEmuDriver() {
+        File drivers_file = new File("/proc/tty/drivers");
+        if (drivers_file.exists() && drivers_file.canRead()) {
+            byte[] data = new byte[(int) drivers_file.length()];
+            try {
+                InputStream is = new FileInputStream(drivers_file);
+                is.read(data);
+                is.close();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+
+            String driver_data = new String(data);
+            for (String known_qemu_driver : FindEmulator.known_qemu_drivers) {
+                if (driver_data.indexOf(known_qemu_driver) != -1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean hasKnownPhoneNumber(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        String phoneNumber = telephonyManager.getLine1Number();
+
+        for (String number : known_numbers) {
+            if (number.equalsIgnoreCase(phoneNumber)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean hasKnownDeviceId(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        String deviceId = telephonyManager.getDeviceId();
+
+        for (String known_deviceId : known_device_ids) {
+            if (known_deviceId.equalsIgnoreCase(deviceId)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean hasKnownImsi(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String imsi = telephonyManager.getSubscriberId();
+
+        for (String known_imsi : known_imsi_ids) {
+            if (known_imsi.equalsIgnoreCase(imsi)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasEmulatorBuild(Context context) {
+        String BOARD = android.os.Build.BOARD; // The name of the underlying board, like "unknown".
+        String BOOTLOADER = android.os.Build.BOOTLOADER; // The system bootloader version number.
+        String BRAND = android.os.Build.BRAND; // The brand (e.g., carrier) the software is customized for, if any.
+                                               // "generic"
+        String DEVICE = android.os.Build.DEVICE; // The name of the industrial design. "generic"
+        String HARDWARE = android.os.Build.HARDWARE; // The name of the hardware (from the kernel command line or
+                                                     // /proc). "goldfish"
+        String MODEL = android.os.Build.MODEL; // The end-user-visible name for the end product. "sdk"
+        String PRODUCT = android.os.Build.PRODUCT; // The name of the overall product.
+        if ((BOARD == "unknown") || (BOOTLOADER == "unknown") || (BRAND == "generic") || (DEVICE == "generic")
+                        || (MODEL == "sdk") || (PRODUCT == "sdk") || (HARDWARE == "goldfish")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isOperatorNameAndroid(Context paramContext) {
+        String szOperatorName = ((TelephonyManager) paramContext.getSystemService("phone")).getNetworkOperatorName();
+        boolean isAndroid = szOperatorName.equalsIgnoreCase("android");
+        return isAndroid;
+    }
 }
